ca532127c74f1484e536f9dd171d109d87dd8651
Molnar Emese <mse212002@gmail.com>
2020-01-21 10:45:10 +0200

    Removed unneunnecessary files

--- src/index.js
+++ src/index.js
@@ -1,6 +1,1 @@
-import React from 'react';
-import './index.css';
-import ReactDOM from 'react-dom';
-import App from './components/App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
