5539eab3713e154fcc6acbd857fa035c29d00cd4
Mesi <mse212002@gmail.com>
2020-02-24 12:39:31 +0200

    :art: fixing linter errors

--- src/logic/calculate.js
+++ src/logic/calculate.js
@@ -7,21 +7,21 @@ const calculate = (btnName, dataObj) => {
     AC: 0,
     '+/-': -1,
     '%': 0.01,
-  }
+  };
 
-  switch(btnName) {
+  switch (btnName) {
     case 'AC':
       total = null;
       next = null;
       operation = null;
       return { total, next, operation };
     case '+/-':
-      if(next) {
+      if (next) {
         next = operate(next, ops[btnName], 'X');
-      } else if(total) {
+      } else if (total) {
         total = operate(total, ops[btnName], 'X');
-      };
-      return {total, next, operation}
+      }
+      return { total, next, operation }
     case '+':
     case '-':
     case 'X':
@@ -29,21 +29,26 @@ const calculate = (btnName, dataObj) => {
       next = null;
       operation = btnName;
       total = operate(total, next, operation);
-      return {total, next, operation};
+      return { total, next, operation };
     case '%':
-      if(!next) {
+      if (!next) {
         total = operate(total, next = null, btnName);
-        return {total, next, btnName};
       } else {
         total = next;
         next = null;
-        next = operate(total, next, btnName)
-        return {total, next, btnName};
-      }
+        next = operate(total, next, btnName);
+        }
+      return { total, next, btnName };
     case '=':
       next = null;
       total = operate(total, next, operation);
-      return {total, next, operation}
+      return { total, next, operation };
+    default:
+      if (total && !operation) {
+        total = null;
+      } else if (!next) {
+        next = btnName;
+      }
   }
 };
 
