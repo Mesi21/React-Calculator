9131901bf53048ec2061f464f4877736cea5b80d
Molnar Emese <mse212002@gmail.com>
2020-05-05 11:33:47 +0300

    :construction: added handleClick to Button component

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,23 +2,35 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import '../style/Button.css';
 
-function Button({ btnName, color, wide }) {
+function Button({
+  btnName, color, wide, clickHandler,
+}) {
   const color2 = 'lightGray';
-
+  const handleClick = (btnName) => clickHandler(btnName);
   const buttonStyle = {
     width: wide ? '50%' : '25%',
-    backgroundColor: (btnName === 'X'
-      || btnName === '÷' || btnName === '-' || btnName === '+'
-      || btnName === '=') ? color : color2,
+    backgroundColor:
+      btnName === 'X'
+      || btnName === '÷'
+      || btnName === '-'
+      || btnName === '+'
+      || btnName === '='
+        ? color
+        : color2,
     border: '1px solid #A0A0A0',
   };
-  return <button type="button" style={buttonStyle}>{btnName}</button>;
+  return (
+    <button type="button" style={buttonStyle} onClick={handleClick(btnName)}>
+      {btnName}
+    </button>
+  );
 }
 
 Button.propTypes = {
   btnName: PropTypes.string.isRequired,
   wide: PropTypes.bool,
   color: PropTypes.string,
+  clickHandler: PropTypes.func.isRequired,
 };
 
 Button.defaultProps = {
